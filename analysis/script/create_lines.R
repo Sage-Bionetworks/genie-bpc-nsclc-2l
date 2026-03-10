cf8d4ccd0f7026facd71fe8b0323702045ee883e
alexpaynter <alexcpaynter@gmail.com>
2026-03-10 16:21:43 -0700

    Adding functions for cohort build

--- analysis/script/create_lines.R
+++ analysis/script/create_lines.R
@@ -7,9 +7,15 @@ bpc_dat_path <- path('data-raw', 'NSCLC', '3.1-consortium')
 ca_ind <- readr::read_csv(path(bpc_dat_path, 'cancer_level_dataset_index.csv'))
 reg <- readr::read_csv(path(bpc_dat_path, 'regimen_cancer_level_dataset.csv'))
 
-basic_line_of_therapy(
+# function borrowed from genie-rw-response project.
+lot <- basic_line_of_therapy(
   ca_ind = ca_ind,
   reg = reg,
   verbose = TRUE,
   remove_duplicates = TRUE
+)
+
+readr::write_rds(
+  lot,
+  here('data', 'lot.rds')
 )
